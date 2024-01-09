28368dfebad68124aa7eb364b814d782e91a7642
Akhil Panwar <akhilpanwar2722908@gmail.com>
2024-01-09 15:45:43 +0530

    worked with leave and attendance

--- src/pages/attendance/index.js
+++ src/pages/attendance/index.js
@@ -1,47 +1,57 @@
 import React from "react"
 import Image from 'next/image'
+import { useRouter } from "next/router"
 import calendar from '../../assets/calendar.png'
 import picture from '../../assets/picture.png'
-import { useEffect } from 'react';
 import './style.css'
 
-
 export default function Attendance() {
-    function addAttendance(){
 
+  
+
+    const router = useRouter();
+    function addAttendance() {
+        router.push("/leave");
+    }
+
+    function Arrow() {
+        router.push("/home");
     }
 
     return (
 
-        <div>
-            <h1> <svg xmlns="http://www.w3.org/2000/svg" style={{float:"left"}} width="24" height="24" viewBox="0 0 24 24" fill="none">
-  <path d="M19 12H5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M12 19L5 12L12 5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg> 
-                <Image src={picture} style={{borderRadius:50, float: "right", marginTop:-3}}/> 
-            ATTENDANCE 
-             </h1>
-            <br/> <br/> <br/>
+        <>
+            <div className="head">
+                <h1>
+                    {/* <Image src={arrow}  style={{borderRadius:50, float: "left"}} onClick={Arrow} />   */}
+                    <svg xmlns="http://www.w3.org/2000/svg" style={{ float: "left" }} onClick={Arrow} width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <path d="M19 12H5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M12 19L5 12L12 5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <Image src={picture} style={{ borderRadius: 50, float: "right", marginTop: -3 }} /> ATTENDANCE
+                </h1>
+            </div>
             <form>
-                <div className="container">
-                    <br />  
-                    <select className="date-list" >
-                        <option value="Date">Jan </option>
-                        <option> Feb </option>
-                        <option> Mar</option>
-                        <option> Apr</option>
-                        <option> May</option>
-                        <option> Jun </option>
-                        <option> Jul</option>
-                        <option> Aug</option>
-                        <option> Sep</option>
-                        <option> Oct </option>
-                        <option> Nov</option>
-                        <option> Dec</option>
-                    </select>
+                <div className="container" >
+                    <div className="month">
+                        <select className="date-list" >
+                            <option value="Date">Jan </option>
+                            <option> Feb </option>
+                            <option> Mar</option>
+                            <option> Apr</option>
+                            <option> May</option>
+                            <option> Jun </option>
+                            <option> Jul</option>
+                            <option> Aug</option>
+                            <option> Sep</option>
+                            <option> Oct </option>
+                            <option> Nov</option>
+                            <option> Dec</option>
+                        </select>
+                    </div>
                     <div className="year">
-                        <select className="Year-list" style={{background: 'rgba(54, 114, 139, 0.10)'}} >
-                        <option value="Date">2024 </option>
+                        <select className="Year-list" style={{ background: 'rgba(54, 114, 139, 0.10)' }} >
+                            <option value="Date">2024 </option>
                             <option >2023 </option>
                             <option >2022 </option>
                             <option >2021 </option>
@@ -49,7 +59,6 @@ export default function Attendance() {
                     </div>
                 </div>
             </form>
-            <br /> <br />
             <div className="table-list">
                 <table>
                     <tbody>
@@ -58,25 +67,23 @@ export default function Attendance() {
                             <th>Status</th>
                         </tr>
                         <tr>
-                            <td> <Image src={calendar} style={{borderRadius:50}}/> 4-Jan-2024</td>
-                            <td  ><span className= "prs"> Present </span></td>
+                            <td> <Image src={calendar} style={{ borderRadius: 50 }} /> 4-Jan-2024</td>
+                            <td  ><span className="prs"> Present </span></td>
                         </tr>
                         <tr>
-                            <td> <Image src={calendar} style={{borderRadius:50}}/> 4-Jan-2024</td>
+                            <td> <Image src={calendar} style={{ borderRadius: 50 }} /> 4-Jan-2024</td>
                             <td> <span className="lev"> On Leave </span></td>
                         </tr>
                         <tr>
-                            <td> <Image src={calendar} style={{borderRadius:50}}/> 4-Jan-2024</td>
+                            <td> <Image src={calendar} style={{ borderRadius: 50 }} /> 4-Jan-2024</td>
                             <td> <span className="abs">Absent </span></td>
                         </tr>
-                    </tbody>          
+                    </tbody>
                 </table>
-
             </div>
-            <br /> <br />
             <div className="btn-atd">
-                <button className="btn" type="submit" onClick={addAttendance}> ADD  ATTENDANCE </button>
+                <button className="btn" type="submit" onClick={addAttendance}> ADD ATTENDANCE </button>
             </div>
-        </div>
+        </>
     )
 }
--- src/pages/leave/index.js
+++ src/pages/leave/index.js
@@ -1,7 +1,93 @@
 import Image from 'next/image'
+import React from "react"
+import { useRouter } from "next/router"
+import calendar from '../../assets/calendar.png'
+import picture from '../../assets/picture.png'
+import './style.css'
 
 export default function Leave() {
+
+  const router = useRouter();
+  function applyLeave() {
+    router.push("/home");
+  }
+  function Arrow() {
+    router.push("/attendance");
+  }
+
   return (
-   <div>LEAVE PAGE</div>
+    <>
+      <div className="head">
+
+        <h1>
+          <svg xmlns="http://www.w3.org/2000/svg" style={{ float: "left" }} onClick={Arrow} width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path d="M19 12H5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M12 19L5 12L12 5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+          <Image src={picture} style={{ borderRadius: 50, float: "right", marginTop: -3 }} />
+          LEAVES
+        </h1>
+
+
+      </div>
+
+      <form>
+        <div className="container" >
+          <div className="month">
+            <select className="date-list" >
+              <option value="Date">Jan </option>
+              <option> Feb </option>
+              <option> Mar</option>
+              <option> Apr</option>
+              <option> May</option>
+              <option> Jun </option>
+              <option> Jul</option>
+              <option> Aug</option>
+              <option> Sep</option>
+              <option> Oct </option>
+              <option> Nov</option>
+              <option> Dec</option>
+            </select>
+          </div>
+          <div className="year">
+            <select className="Year-list" style={{ background: 'rgba(54, 114, 139, 0.10)' }} >
+              <option value="Date">2024 </option>
+              <option >2023 </option>
+              <option >2022 </option>
+              <option >2021 </option>
+            </select>
+          </div>
+        </div>
+      </form>
+      <div className="table-list">
+        <table>
+          <tbody>
+            <tr>
+              <th>From</th>
+              <th> To </th>
+              <th>Status</th>
+            </tr>
+            <tr>
+              <td> <Image src={calendar} style={{ borderRadius: 50 }} /> 4-Jan-2024</td>
+              <td> 6-Jan-2024</td>
+              <td  ><span className="apr"> Approved </span></td>
+            </tr>
+            <tr>
+              <td> <Image src={calendar} style={{ borderRadius: 50 }} /> 4-Jan-2024</td>
+              <td> 6-Jan-2024</td>
+              <td> <span className="pen"> Pending </span></td>
+            </tr>
+            <tr>
+              <td> <Image src={calendar} style={{ borderRadius: 50 }} /> 4-Jan-2024</td>
+              <td> 6-Jan-2024</td>
+              <td> <span className="cnl">Cancel </span></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="btn-atd">
+        <button className="btn" type="submit" onClick={applyLeave}> APPLY LEAVE </button>
+      </div>
+    </>
   )
 }
